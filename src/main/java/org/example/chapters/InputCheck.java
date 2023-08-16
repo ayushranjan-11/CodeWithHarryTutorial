7143ddf4e393e03f150c5563ac9983a918f2e4ab
ayush <ayush.ranjan921@gmail.com>
2023-08-17 01:25:15 +0530

    Improved folder structure

--- src/main/java/org/example/chapters/InputCheck.java
+++ src/main/java/org/example/chapters/InputCheck.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.example.chapters;
 
 import java.util.Scanner;
 
--- src/main/java/org/example/chapters/Strings.java
+++ src/main/java/org/example/chapters/Strings.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.example.chapters;
 
 public class Strings {
     public static void main(String[] args) {

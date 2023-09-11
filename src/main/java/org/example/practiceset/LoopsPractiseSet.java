f47a452fefd5abd754d715ba825f039cefa2b1c1
ayushranjan-11 <ayush.ranjan921@gmail.com>
2023-09-12 00:48:43 +0530

    Updated the loop practise set with given problems in the course

--- src/main/java/org/example/practiceset/LoopsPractiseSet.java
+++ src/main/java/org/example/practiceset/LoopsPractiseSet.java
@@ -3,6 +3,9 @@ package org.example.practiceset;
 public class LoopsPractiseSet {
     public static void main(String[] args) {
         patternPrint();
+        evenNumbersAddition(10);
+        multiplicationTable(12);
+        multiplicationTableInReverse(10);
     }
 
     public static void patternPrint() {
@@ -33,6 +36,30 @@ public class LoopsPractiseSet {
                 System.out.print("*");
             }
             System.out.println();
+        }
+    }
+
+    public static void evenNumbersAddition(int n) { // n = limit
+        int i=0, sum=0;
+        while(i<=n) {
+            if(i%2==0) {
+                System.out.println("Number is "+i);
+                sum+=i;
+            }
+            i++;
+        }
+        System.out.println(sum);
+    }
+
+    public static void multiplicationTable(int n) { //n = number for which table will be written
+        for(int i=1;i<=10;i++) {
+            System.out.println(n+"*"+i+"="+ (n * i));
+        }
+    }
+
+    public static void multiplicationTableInReverse(int n) {
+        for(int i=10; i>=1; i--) {
+            System.out.println(n+"*"+i+"="+(n*i));
         }
     }
 }

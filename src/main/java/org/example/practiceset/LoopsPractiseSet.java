5c20a442ba6025f031f27aa175021da9e1dbd0c3
ayushranjan-11 <ayush.ranjan921@gmail.com>
2023-09-13 01:18:29 +0530

    Updated Loop practise set class with problems and solutions, also added alternate answers for efficiency

--- src/main/java/org/example/practiceset/LoopsPractiseSet.java
+++ src/main/java/org/example/practiceset/LoopsPractiseSet.java
@@ -2,10 +2,13 @@ package org.example.practiceset;
 
 public class LoopsPractiseSet {
     public static void main(String[] args) {
-        patternPrint();
-        evenNumbersAddition(10);
-        multiplicationTable(12);
-        multiplicationTableInReverse(10);
+
+        // All the code written in this class can be more optimised if we can add input check.
+//        patternPrint();
+        evenNumbersAddition(4);
+//        multiplicationTable(12);
+//        multiplicationTableInReverse(10);
+//        factorialOfNumber(5);
     }
 
     public static void patternPrint() {
@@ -39,16 +42,25 @@ public class LoopsPractiseSet {
         }
     }
 
-    public static void evenNumbersAddition(int n) { // n = limit
-        int i=0, sum=0;
-        while(i<=n) {
+    public static void evenNumbersAddition(int n) { // n = count of even numbers
+        int i=0, sum=0, tempN = n;
+        while(tempN>0) {
             if(i%2==0) {
                 System.out.println("Number is "+i);
-                sum+=i;
+                sum=sum+i;
+                tempN--; //Here we are decreasing the count only after finding even number successfully.
             }
             i++;
+
         }
         System.out.println(sum);
+
+//        //Alternate
+//        int sum = 0;
+//        for (int i =0;i<n;i++) {
+//            sum = sum+((2*i));
+//        }
+//        System.out.println("Sum of "+n+" even numbers is "+sum);
     }
 
     public static void multiplicationTable(int n) { //n = number for which table will be written
@@ -62,4 +74,17 @@ public class LoopsPractiseSet {
             System.out.println(n+"*"+i+"="+(n*i));
         }
     }
+
+    public static void factorialOfNumber(int number) {
+        int result = 1,tempNumber=number;
+        if (tempNumber>0) {
+            while(tempNumber>0) {
+                result *= tempNumber;
+                tempNumber--;
+            }
+        }
+        System.out.println("factorial of number "+number+" is "+result);
+    }
+
+
 }

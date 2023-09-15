2e88bca2f46927fa388ffb01a48e09481cb5c559
ayushranjan-11 <ayush.ranjan921@gmail.com>
2023-09-16 01:07:25 +0530

    Added loop class and included some genreal methods

---
+++ src/main/java/org/example/chapters/Array.java
@@ -0,0 +1,44 @@
+package org.example.chapters;
+
+public class Array {
+    public static void main(String[] args) {
+        arrayFirstProgram();
+        arraySecondProgram();
+        arrayReverseOrderPrint();
+    }
+
+    public static void arrayFirstProgram() {
+        int[] a = {100, 200, 300, 400, 500, 600};   // This type of array is Declare + Initialize
+        for (int i = 0; i<a.length;i++){
+            System.out.println(a[i]);
+        }
+    }
+
+    public static void arraySecondProgram() {
+        int[] b;
+        b = new int[11];
+        int tempSize = b.length, ii =0;
+
+        for(int i = 0; i<tempSize; i++) {
+            b[i]=2*i;
+        }
+
+        while(tempSize>0) {
+
+            System.out.println(b[ii]);
+            ii++;
+            tempSize--;
+        }
+    }
+
+    public static void arrayReverseOrderPrint() {
+        int [] c =new int[5];
+        for (int i = 0; i<c.length; i++) {
+            c[i] = i;
+        }
+
+        for(int j = c.length-1; j>=0; j--) {
+            System.out.println(c[j]);
+        }
+    }
+}

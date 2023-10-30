74b5c8ea145b8a9b797b898e941c0f16a9a4abf2
ayushranjan-11 <ayush.ranjan921@gmail.com>
2023-10-31 01:50:51 +0530

    Wrote fibonacci series in MethodPractise.java class

--- src/main/java/org/example/practiceset/MethodPractise.java
+++ src/main/java/org/example/practiceset/MethodPractise.java
@@ -19,5 +19,19 @@ public class MethodPractise {
         int[]y = {1,2,3,4,5};
         method2(y);
         System.out.println(y[0]);
+        fibonacciSeries(10);
+    }
+
+    //Fibonacci series
+    static void fibonacciSeries(int limit) {
+        int start=0, start1 = 1, calculation=0;
+
+        for (int i= 0; i<limit; i++) {
+            calculation+=start1;
+            System.out.print(" "+calculation);
+            start=start1;
+            start1=calculation;
+            calculation=start;
+        }
     }
 }

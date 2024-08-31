510de18a217b005bb729fba0bf0f4fe9af651ea3
AYUSH RANJAN <ayush.ranjan921@gmail.com>
2024-09-01 00:03:52 +0530

    Updated the Constructor class with values and no values passing

--- src/main/java/org/learning/oopsConcepts/Constructors.java
+++ src/main/java/org/learning/oopsConcepts/Constructors.java
@@ -5,6 +5,7 @@ class InnerClassForConstructor {
     private int roll;
     private String name;
 
+    // This is a constructor for this class
     public InnerClassForConstructor() {
         roll = 50;
         name = "Learning";
@@ -27,12 +28,57 @@ class InnerClassForConstructor {
     }
 }
 
+//Also we can do multiple inner class in JAVA
+class ConstructorWithValues {
+    //This class will have values and it will also be updated.
+
+    private int employeeSalary;
+    private String employeeDesignation;
+
+    public ConstructorWithValues(int updatedSalary, String desginationChange) {
+        this.employeeDesignation = desginationChange;
+        this.employeeSalary = updatedSalary;
+    }
+
+    public void setSalary(int salaryInput) {
+        employeeSalary = salaryInput;
+    }
+
+    public int getSalary(){
+        return employeeSalary;
+    }
+
+    public void setDesignation(String updatedDesignation) {
+        employeeDesignation = updatedDesignation;
+    }
+
+    public String getCurrentDesignation(){
+        return employeeDesignation;
+    }
+    
+}
 public class Constructors {
 
     public static void main(String[] args) {
 
         InnerClassForConstructor innerClassForConstructor = new InnerClassForConstructor();
+        /*Here name and roll are getting prinited without even intializing or calling the setters,
+         as in constructors the values are set for them already */
         System.out.println(innerClassForConstructor.getName());
         System.out.println(innerClassForConstructor.getRoll());
+        innerClassForConstructor.setName("No argument constructor name display");
+        innerClassForConstructor.setRoll(10);
+
+        System.out.println(innerClassForConstructor.getName());
+        System.out.println(innerClassForConstructor.getRoll());
+        
+        //Constructor with values initialied class working
+        ConstructorWithValues constructorWithValues = new ConstructorWithValues(0, "null");
+        constructorWithValues.setDesignation("null from set method");
+        constructorWithValues.setSalary(0);
+        System.out.println(constructorWithValues.getSalary());
+        System.out.println(constructorWithValues.getCurrentDesignation());
+
+
     }
 }

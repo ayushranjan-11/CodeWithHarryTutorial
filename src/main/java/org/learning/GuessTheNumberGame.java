0680c1f3e89c7181ac8a8e822818f70d24e8f5e1
ayushranjan-11 <ayush.ranjan@gmail.com>
2024-09-06 11:04:59 +0530

    Also included message on input difference and generated random number

--- src/main/java/org/learning/GuessTheNumberGame.java
+++ src/main/java/org/learning/GuessTheNumberGame.java
@@ -3,13 +3,12 @@ package org.learning;
 import java.util.Scanner;
 
 public class GuessTheNumberGame {
-
     private static int countStartNumber = 1; // Integer declaration for total guess count
 
     public static void main(String[] args) {
 
         GuessNumber guessNumber = new GuessNumber();
-        System.out.println("Computer Generated no. was: " + guessNumber.getGeneratedRandomNumber());
+        //System.out.println("Computer Generated no. was: " + guessNumber.getGeneratedRandomNumber());
         System.out.println("Provide your input for guess");
         guessGame(guessNumber);
         System.out.println("Computer Generated no. was: " + guessNumber.getGeneratedRandomNumber());
@@ -17,8 +16,7 @@ public class GuessTheNumberGame {
 
     }
 
-    // TODO: User provided number compare to generated number and let user know
-    // large or small number from the generated number difference
+    // TODO: User provided number compare to generated number and let user know large or small number from the generated number difference
 
     public static void guessGame(GuessNumber guessNumber) {
         Scanner takeInput = new Scanner(System.in);
@@ -34,6 +32,12 @@ public class GuessTheNumberGame {
                 if (guessNumber.checkUserGuess(userProvidedInput)) {
                     System.out.println("Congratulation on getting correct guess!!");
                 } else {
+                    if (userProvidedInput > guessNumber.getGeneratedRandomNumber()) {
+                        System.out.println("Guessed number is greater than the number");
+                    }
+                    if (userProvidedInput < guessNumber.getGeneratedRandomNumber()) {
+                        System.out.println("Guessed number is smaller than the number");
+                    }
                     System.out.println("Take another guess");
                     guessGame(guessNumber);
                 }
@@ -48,7 +52,7 @@ public class GuessTheNumberGame {
 }
 
 class GuessNumber {
-    private int userGuess;
+    //private int userGuess;
     private int guessCount;
     private int generatedRandomNumber;
 
@@ -71,9 +75,6 @@ class GuessNumber {
 
     public boolean checkUserGuess(int userGuessedNumber) {
 
-        if (userGuessedNumber == generatedRandomNumber) {
-            return true;
-        }
-        return false;
+        return userGuessedNumber == generatedRandomNumber;
     }
 }

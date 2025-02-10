4f5be6cd4c33911ca89bcdf992acb2c931b63fd0
ayushranjan-11 <ayush.ranjan@gmail.com>
2025-02-10 23:18:45 +0530

    started course again with inheritance learning

--- src/main/java/org/learning/oopsConcepts/InheritanceLearning.java
+++ src/main/java/org/learning/oopsConcepts/InheritanceLearning.java
@@ -15,5 +15,8 @@ class classInheritingClass extends InheritanceLearning {
     static void inheritanceCheck() {
         classInheritingClass classInheritingClass = new classInheritingClass();
         System.out.println(classInheritingClass.testString + " but was called from child class");
+        System.out.println("Now changing the string value in child class for the parent class variable");
+        classInheritingClass.testString = "made a modification on child class";
+        System.out.println(classInheritingClass.testString + " value changed from child class");
     }
 }
